cc0a2416834bf8f722c9bec60cf427c518067f2d
Yazdani <yaz4dani@gmail.com>
2019-06-20 11:05:56 +0600

    Drawer Added

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -128,6 +128,7 @@ class _HomeState extends State<Home> {
                           ),
                         ),
                       ),
+                      
                       Padding(
                         padding: const EdgeInsets.all(8.0),
                         child: new Text("Loren Ipsum",
